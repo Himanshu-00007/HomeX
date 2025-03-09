018898dc21c36e709da502c4cb2e433f6b67bb9e
Himanshu pandey <147255961+Himanshu-00007@users.noreply.github.com>
2025-03-09 10:09:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+if(process.env.NODE_ENV !="production"){
+  require('dotenv').config();
+}
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const listing = require("./models/listing.js");
+const review = require("./models/review.js");
+const { listingschema, reviewschema} = require("./schema.js");
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(methodOverride("_method"));
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
+const MongoStore = require('connect-mongo');
+const session=require("express-session");
+const flash=require("connect-flash");
+const passport=require("passport");
+const localStrategy=require("passport-local");
+const User=require("./models/user.js");
+const dburl=process.env.ATLASDB_URL;
+// Connect to MongoDB
+main()
+  .then(() => {
+    console.log("DB is connected");
+  })
+  .catch((err) => {
+    console.log("DB connection error:", err);
+  });
+
+async function main() {
+  await mongoose.connect(dburl);
+}
+const store=MongoStore.create({
+  mongoUrl:dburl,
+  crypto:{
+    secret: process.env.SECRET
+  },
+  touchafter:24*3600,
+});
+store.on("error",()=>{
+  console.log("ERROR IN MONGO SESSION STORE",err);
+});
+const sessionOptions = {
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 7 * 24 * 60 * 60 * 1000,  // 7 days in milliseconds
+    httpOnly: true,
+  },
+};
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req,res,next)=>{
+  res.locals.success=req.flash("success");
+  res.locals.error=req.flash("error");
+  res.locals.currUser=req.user;
+  next();
+});
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
+// Reviews
+// 404 Handler
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).send(message);
+});
+
+// Start Server
+app.listen(8080, () => {
+  console.log("Server is listening at port 8080");
+});
